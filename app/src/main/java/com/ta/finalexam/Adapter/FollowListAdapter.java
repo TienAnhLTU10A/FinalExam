f80a6532b858863d8af7299dd0b572d02c576f59
TienAnhLTU10A <koihimemusou1993@gmail.com>
2016-10-14 20:03:03 +0900

    Xong Toolbar giong LeCub app

---
+++ app/src/main/java/com/ta/finalexam/Adapter/FollowListAdapter.java
@@ -0,0 +1,34 @@
+package com.ta.finalexam.Adapter;
+
+import android.view.ViewGroup;
+
+import com.ta.finalexam.Adapter.ViewHolder.FollowListViewHolder;
+import com.ta.finalexam.DummyFollowItem;
+
+import java.util.List;
+
+import vn.app.base.adapter.AdapterWithItemClick;
+import vn.app.base.util.UiUtil;
+
+/**
+ * Created by Veteran Commander on 10/14/2016.
+ */
+
+public class FollowListAdapter extends AdapterWithItemClick<FollowListViewHolder> {
+    public List<DummyFollowItem> dummyFollowItemList;
+    @Override
+    public FollowListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new FollowListViewHolder(UiUtil.inflate(parent, FollowListViewHolder.LAYOUT_ID, false));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dummyFollowItemList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(FollowListViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        holder.bind(dummyFollowItemList.get(position));
+    }
+}
--- app/src/main/java/com/ta/finalexam/MainActivity.java
+++ app/src/main/java/com/ta/finalexam/MainActivity.java
@@ -1,13 +1,154 @@
 package com.ta.finalexam;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.ta.finalexam.Bean.HeaderControlBean;
+import com.ta.finalexam.Constant.HeaderOption;
+import com.ta.finalexam.Fragment.DummyFragment;
+
+import butterknife.BindView;
+import vn.app.base.activity.CommonActivity;
+import vn.app.base.util.StringUtil;
+import vn.app.base.util.UiUtil;
+
+public class MainActivity extends CommonActivity {
+
+    @BindView(R.id.toolbar)
+    RelativeLayout rlToolbar;
+
+    @BindView(R.id.headerMenu)
+    ImageView ivMenu;
+
+    @BindView(R.id.headerBack)
+    ImageView ivBack;
+
+    @BindView(R.id.tv_delete)
+    TextView tvDelete;
+
+    @BindView(R.id.tv_update)
+    TextView tvUpdate;
+
+    @BindView(R.id.headerTitle)
+    TextView tvTitle;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected String getNoConnectionMessage() {
+        return getString(R.string.dialog_error_no_connection);
+    }
+
+    @Override
+    protected String getErrorAPIMessage() {
+        return getString(R.string.dialog_error_no_connection);
+    }
+
+    @Override
+    protected String getTimeOutMessage() {
+        return getString(R.string.dialog_error_timeout);
+    }
+
+    @Override
+    public int setContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+        setUpInitScreen(new DummyFragment(), null);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onCommonDataHandle(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onCommonUIHandle(Bundle bundle) {
+        if (bundle == null) {
+            return;
+        }
+        if (bundle.containsKey(HeaderOption.HEADER_CONTROL)) {
+            HeaderControlBean headerControlBean = bundle.getParcelable(HeaderOption.HEADER_CONTROL);
+            if (headerControlBean != null) {
+                handleHeaderUI(headerControlBean);
+            }
+        }
+    }
+
+    private void handleHeaderUI(HeaderControlBean headerControlBean) {
+        if (headerControlBean.headerOptions != null && headerControlBean.headerOptions.length > 0) {
+            if (headerControlBean.headerOptions[0] == HeaderOption.SHOW_HEADER) {
+                UiUtil.showView(rlToolbar);
+            } else if (headerControlBean.headerOptions[0] == HeaderOption.HIDE_HEADER) {
+                UiUtil.hideView(rlToolbar, true);
+            }
+            if (headerControlBean.headerOptions.length > 1 && headerControlBean.headerOptions[1] > 0) {
+                handleLeftIcon(headerControlBean.headerOptions[1]);
+                if (headerControlBean.headerOptions.length > 2 && headerControlBean.headerOptions[2] > 0) {
+                    handleRightIcon(headerControlBean.headerOptions[2]);
+                } else {
+                    showAndHideIconRight(null);
+                }
+            } else {
+                showAndHideIconLeft(ivMenu);
+            }
+        }
+        if (StringUtil.checkStringValid(headerControlBean.title)) {
+            StringUtil.displayText(headerControlBean.title, tvTitle);
+        }
+    }
+
+    private void handleLeftIcon(int value) {
+        switch (value) {
+            case HeaderOption.LEFT_NO_OPTION:
+                showAndHideIconLeft(null);
+                break;
+            case HeaderOption.LEFT_MENU:
+                showAndHideIconLeft(ivMenu);
+                break;
+            case HeaderOption.LEFT_BACK:
+                showAndHideIconLeft(ivBack);
+                break;
+        }
+    }
+
+    private void handleRightIcon(int value) {
+        switch (value) {
+            case HeaderOption.RIGHT_NO_OPTION:
+                showAndHideIconRight(null);
+                break;
+            case HeaderOption.RIGHT_DELETE:
+                showAndHideIconRight(tvDelete);
+                break;
+            case HeaderOption.RIGHT_UPDATE:
+                showAndHideIconRight(tvUpdate);
+                break;
+        }
+    }
+
+    private void showAndHideIconLeft(View target) {
+        showOrHide(ivMenu, target);
+        showOrHide(ivBack, target);
+    }
+
+    private void showAndHideIconRight(View target) {
+        showOrHide(tvDelete, target);
+        showOrHide(tvUpdate, target);
+    }
+
+    protected void showOrHide(View subject, View target) {
+        subject.setVisibility(subject == target ? View.VISIBLE : View.GONE);
     }
 }
